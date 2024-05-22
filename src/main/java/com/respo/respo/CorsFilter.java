c9ee5c6605ace81f55938d186ef6dba48ea26ca3
daestortion <danielgilbertd@gmail.com>
2024-05-22 15:34:14 +0800

    origin

--- src/main/java/com/respo/respo/CorsFilter.java
+++ src/main/java/com/respo/respo/CorsFilter.java
@@ -17,7 +17,7 @@ public class CorsFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
 
-        response.setHeader("Access-Control-Allow-Origin", "https://main--wheelsongo.netlify.app/"); // Replace with your frontend URL
+        response.setHeader("Access-Control-Allow-Origin", "https://main--wheelsongo.netlify.app"); // Replace with your frontend URL
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         response.setHeader("Access-Control-Allow-Headers", "*");
         response.setHeader("Access-Control-Max-Age", "3600");
