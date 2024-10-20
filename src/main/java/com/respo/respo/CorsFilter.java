6c34560958bc3973c78f46532cc6e243286112a3
Asmodeus0613 <ayadekenneth07@gmail.com>
2024-10-20 17:43:49 +0800

    origin

--- src/main/java/com/respo/respo/CorsFilter.java
+++ src/main/java/com/respo/respo/CorsFilter.java
@@ -1,12 +1,19 @@
 package com.respo.respo;
 
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
@@ -18,7 +25,8 @@ public class CorsFilter implements Filter {
         HttpServletResponse res = (HttpServletResponse) response;
         HttpServletRequest req = (HttpServletRequest) request;
 
-        res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        // Update to allow both localhost and Vercel frontend
+        res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000, https://wheels-on-go-front-end.vercel.app");
         res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT, PATCH");
         res.setHeader("Access-Control-Max-Age", "3600");
         res.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
