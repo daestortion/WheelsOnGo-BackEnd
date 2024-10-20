d917af24f5a642d7d2f19e2f0ab19d3ac2e0ad4b
Asmodeus0613 <ayadekenneth07@gmail.com>
2024-10-20 17:02:59 +0800

    origin

---
+++ src/main/java/com/respo/respo/Configuration/WebSocketConfig.java
@@ -0,0 +1,18 @@
+package com.respo.respo.Configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Override
+    public void registerWebSocketHandlers(@NonNull WebSocketHandlerRegistry registry) {
+        registry.addHandler(new ChatWebSocketHandler(), "/ws/chat/{chatId}")  // Adding chatId as a path variable
+                .setAllowedOrigins("*");
+    }
+}
