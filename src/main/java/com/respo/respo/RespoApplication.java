3ba4f5276fe822c7b2b6479094e3c439635fede7
daestortion <danielgilbertd@gmail.com>
2024-12-08 19:15:54 +0800

    Revert "idle timeout adjustment"
    This reverts commit d0220ac35b1399d543cad7865ef1bebef696781b.

--- src/main/java/com/respo/respo/RespoApplication.java
+++ src/main/java/com/respo/respo/RespoApplication.java
@@ -7,9 +7,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import java.lang.management.ManagementFactory;
-import java.lang.management.MemoryMXBean;
-import java.lang.management.MemoryUsage;
 
 @SpringBootApplication
 @EnableCaching
@@ -19,11 +16,6 @@ public class RespoApplication implements CommandLineRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(RespoApplication.class, args);
 		System.out.println("HUMANA ME CAPSTONE 1!!!!!!!!!!!!!!!!!!");
-		MemoryMXBean memoryMXBean = ManagementFactory.getMemoryMXBean();
-        MemoryUsage heapMemoryUsage = memoryMXBean.getHeapMemoryUsage();
-        System.out.println("Initial Heap Size: " + heapMemoryUsage.getInit() / (1024 * 1024) + " MB");
-        System.out.println("Max Heap Size: " + heapMemoryUsage.getMax() / (1024 * 1024) + " MB");
-        System.out.println("Used Heap Size: " + heapMemoryUsage.getUsed() / (1024 * 1024) + " MB");
 	}
 	
 	@Override
