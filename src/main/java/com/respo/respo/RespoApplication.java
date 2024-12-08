d0220ac35b1399d543cad7865ef1bebef696781b
daestortion <danielgilbertd@gmail.com>
2024-12-08 18:57:13 +0800

    idle timeout adjustment

--- src/main/java/com/respo/respo/RespoApplication.java
+++ src/main/java/com/respo/respo/RespoApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryMXBean;
+import java.lang.management.MemoryUsage;
 
 @SpringBootApplication
 @EnableCaching
@@ -16,6 +19,11 @@ public class RespoApplication implements CommandLineRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(RespoApplication.class, args);
 		System.out.println("HUMANA ME CAPSTONE 1!!!!!!!!!!!!!!!!!!");
+		MemoryMXBean memoryMXBean = ManagementFactory.getMemoryMXBean();
+        MemoryUsage heapMemoryUsage = memoryMXBean.getHeapMemoryUsage();
+        System.out.println("Initial Heap Size: " + heapMemoryUsage.getInit() / (1024 * 1024) + " MB");
+        System.out.println("Max Heap Size: " + heapMemoryUsage.getMax() / (1024 * 1024) + " MB");
+        System.out.println("Used Heap Size: " + heapMemoryUsage.getUsed() / (1024 * 1024) + " MB");
 	}
 	
 	@Override
